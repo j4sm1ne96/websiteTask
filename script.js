9eaf351cbc6a5270146dd68f31e41fc4fc22071a
Jasmine Taylor <83115753+j4sm1ne96@users.noreply.github.com>
2022-03-09 20:38:36 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', function(){
+    new Glider(document.querySelector('.glider'), {
+        slidesToScroll: 3,
+        slidesToShow: 1,
+        dots: '.dots'
+    });
+  })
+
+    
